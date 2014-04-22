2f0cf37e92309056800f39856cf12c6eb5572199
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-04-23 02:08:03 +0900

    temp save

--- lib/sns_parser/ruby.rb
+++ lib/sns_parser/ruby.rb
@@ -1,7 +1,19 @@
+require 'pp'
+require 'ripper'
+
 class Ruby
   def initialize
   end
 
   def parse(file)
+    result = Ripper.lex(File.open(file))
+
+    result.each do |res|
+      checker(*res[0], res[1], res[2])
+    end
+  end
+
+  def checker( line, column, symbol, value )
+    puts "symbol = #{symbol}, value = #{value}"
   end
 end
--- spec/sns_parser/sns_pserser_spec.rb
+++ spec/sns_parser/sns_pserser_spec.rb
@@ -2,4 +2,10 @@ require 'sns_parser'
 require 'spec_helper'
 
 describe "sns for ruby" do
+  describe "ruby parser" do
+    it "should be file parse" do
+      parser = Ruby.new
+      parser.parse("example/test.rb")
+    end
+  end
 end
